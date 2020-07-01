ca7a0ea080be6f23eecb03dd4fc0842caafa7d08
SadFarm1 <40647186+SadFarm1@users.noreply.github.com>
2020-07-01 00:02:57 -0400

    Added quiz site

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import dotenv
 from dotenv import load_dotenv, find_dotenv
 import pymongo
 from pymongo import MongoClient
+import random
+import datetime
 
 app = Flask(__name__)
 app.secret_key = os.getenv('APP_SECRET')
@@ -123,6 +125,9 @@ def studentProfile():
             student_name = student_account['Name']
             teacher_name = student_account['Teacher']
             session['name'] = student_name
+
+
+
             return render_template('studentprofile.html', name=student_name, teacher_name = teacher_name)
 
         elif request.method == 'POST':
@@ -144,8 +149,7 @@ def usersList():
 
             lencount = len(student_emails)
             count = range(0, lencount)
-            print(student_emails, student_names, student_list)
-            print(count) 
+
 
             return render_template('userslist.html', count = len(student_emails), emails=student_emails, names=student_names)
 
@@ -185,7 +189,7 @@ def newUser():
         name = request.form.get('name')
         password = request.form.get('Password')
 
-        if len(email) > 0 and len(name) > 0 and len(password) > 0:
+        if email is not None and name is not None and password is not None:
 
             post = {'Email': email, 'Password': password, 'Type': 'Student', 'Name': name, 'Teacher': session['name']}
             users_collection.insert_one(post)
@@ -204,15 +208,81 @@ def delUser(email):
     flash('User successfully deleted!')
     return redirect(url_for('usersList'))
 
+question = ''
 
 
+@app.route('/studentprofile/question/<question_number>', methods=['GET', 'POST'])
+def quiz(question_number):
+
+    global question
+    if request.method == 'GET':
+        
+        questions_full_list =  users_collection.find_one({'Type': 'Questions'})
+        question_index = str('Question' + question_number)
+        questions_list = questions_full_list[question_index]
+        question = random.choice(questions_list)
+        question_number_title = f'Question #{int(question_number)}'
+
+        return render_template('question.html', question=question, question_title= question_number_title)
+
+    elif request.method == 'POST':
+        
+        question = question
+        answer_choice = request.form.get('response')
+
+        if answer_choice is None:
+            flash('Please choose an answer!')
+            return redirect(f'/studentprofile/question/{question_number}')
+
+        question_number = int(question_number)
+        current_date = datetime.datetime.today().strftime ('%m/%d/%Y')
+
+  
+
+        if question_number == 1:
+            users_collection.remove({'Date': current_date, 'Name': session['name'], 'Type': 'Answers'})
+
+            post = {'Type': 'Answers', 'Name': session['name'], 'Questions': [question], 'Answers': [answer_choice], 'Date': current_date}
+            users_collection.insert_one(post)
+        elif question_number == 5:
+            student_answers = users_collection.find_one({'Type': 'Answers', 'Name': session['name'], 'Date': current_date})
+            previous_questions = student_answers['Questions']
+            previous_answers = student_answers['Answers']
+
+            previous_questions.append(question)
+            previous_answers.append(answer_choice)
+
+            users_collection.find_one_and_update({'Type': 'Answers', 'Name': session['name'], 'Date': current_date}, {'$set' :{'Questions': previous_questions, 'Answers': previous_answers}})
+
+            flash('Test submitted!')
+            return redirect(url_for('studentProfile'))            
+        
+        else:
+            student_answers = users_collection.find_one({'Type': 'Answers', 'Name': session['name'], 'Date': current_date})
+            previous_questions = student_answers['Questions']
+            previous_answers = student_answers['Answers']
+
+            previous_questions.append(question)
+            previous_answers.append(answer_choice)
+
+            users_collection.find_one_and_update({'Type': 'Answers', 'Name': session['name'], 'Date': current_date}, {'$set' :{'Questions': previous_questions, 'Answers': previous_answers}})
+        
+
+        question_number += 1
+        return redirect(f'/studentprofile/question/{question_number}')
+
+
+
+
+    
 @app.route('/logout', methods=['POST', 'GET'])
 def logout():
     session.pop('email', None)
     session.pop('password', None)
     session.pop('name', None)
     session.pop('type', None)
-    return redirect(url_for('landing'))
+    return redirect(url_for('landing'))    
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port = 80, debug=True)
